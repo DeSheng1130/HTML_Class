1fd285b35f8abfac9ca215bf8723fd06adc22ede
DeSheng <10546006@ntub.edu.tw>
2019-05-18 15:51:50 +0800

    first commit

---
+++ WebDesignForClass-master/echo/src/ntub/EchoHello.java
@@ -0,0 +1,61 @@
+package ntub;
+
+import java.io.IOException;
+import java.util.Enumeration;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class EchoHello
+ */
+@WebServlet("/EchoHello")
+public class EchoHello extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public EchoHello() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		Enumeration<String> enums =  request.getParameterNames();
+		response.getWriter().append("\n");
+		while(enums.hasMoreElements()) {
+			String name = enums.nextElement();
+			String[] values = request.getParameterValues(name);
+			response.getWriter().append(name + ": ");
+			response.getWriter().append(String.join(", ", values));
+			response.getWriter().append("\n");
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		Enumeration<String> enums =  request.getParameterNames();
+		response.getWriter().append("\n");
+		while(enums.hasMoreElements()) {
+			String name = enums.nextElement();
+			String[] values = request.getParameterValues(name);
+			response.getWriter().append(name + ": ");
+			response.getWriter().append(String.join(", ", values));
+			response.getWriter().append("\n");
+		}
+		
+	}
+
+}
